55d21ddf8b621e89fc069d1ee5103c90d54ef2c2
roxeannechico <roseannechico22@gmail.com>
2024-11-04 19:02:12 +0800

    git commit -m "initial commit"

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullName = 'Rose Anne D. Chico';
+let age = '19';
+let favoriteNumber = '22';
+let favoriteColor = 'Purple';
+
+console.log(fullName);
+console.log(age);
+console.log(favoriteNumber);
+console.log(favoriteColor);
